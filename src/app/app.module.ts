7e16c108001dc54758e4b5dcf8df3b4dec033377
Sarah Beaver <sbeaver57@yahoo.com>
2022-10-17 09:52:22 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,7 @@ import { CartComponent } from './cart/cart.component';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDN4PKmeO_gKmV7wBhGMtW4bvnnfi2STS8",
-  authDomain: "candy2-f3465.firebaseapp.com",
-  projectId: "candy2-f3465",
-  storageBucket: "candy2-f3465.appspot.com",
-  messagingSenderId: "92083339333",
-  appId: "1:92083339333:web:e97c19c1cde54a5eeb232c",
-  measurementId: "G-QQYZQ8BSGV"
+
 };
 
 @NgModule({
